16964bda92f7eba1c17718677aeecc26216b9358
madisonhypes <99423292+madisonhypes@users.noreply.github.com>
2022-02-19 09:39:58 -0800

    FINAL VERSION. Just change dir when users run.

--- ECS129-Project.py
+++ ECS129-Project.py
@@ -1,7 +1,67 @@
 import os
 from tracemalloc import start
 
+# REMEMBER:
+# Change dir below to choose the folder you want to read and
+# write to before running this program.
 os.chdir(r'C:\Users\Madison Hypes\Desktop\FOLDER\Code\ECS129Project')
+
+
+def writeOutput():
+    with open("SequenceOutput.txt", "w") as f:
+        f.write(
+            '\n.-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.')
+        f.write('\nWelcome to our ECS129 project for 2022!\n')
+        f.write('\n')
+        f.write('Project made by Madison Hypes, Jason Hu')
+        f.write(', and David Galkowski.\n')
+        f.write('Created on February 19th, 2022\n')
+        f.write('\n')
+        f.write('Our project will read a dna sequence in')
+        f.write(' fasta format, and return\nthe protein made')
+        f.write(' from the longest open reading frame.\n')
+        f.write('\nRESULTS SHOWN BELOW\n-------\n')
+        f.write('Input Sequence:\n' + seq + '\n')
+        f.write('\nReverse Complement Sequence:\n')
+        f.write(reverseComp)
+        f.write('\n\nLargest ORF in Input Sequence:\n')
+        f.write(globalList[0])
+        f.write('\n\nLargest ORF in Reverse Complement ')
+        f.write('Sequence:\n')
+        f.write(globalList[1] + '\n\n')
+        f.write('LARGEST ORF between Input Sequence and ')
+        f.write('Reverse Complement Sequence -\n')
+        if len(globalList[0]) > len(globalList[1]):
+            f.write('ORF from Input Sequence is Larger')
+            f.write(' and is written below:\n')
+            f.write(globalList[0])
+        elif len(globalList[0]) == len(globalList[1]):
+            f.write('ORF from Inputs Sequence is equal length')
+            f.write(' to ORF from Reverse Complement:\n')
+            f.write(globalList[0])
+        else:
+            f.write('ORF from Reverse Complement is Larger')
+            f.write(' and is written below:\n')
+            f.write(globalList[1])
+        f.write('\n\nmRNA Sequence of ORF:\n')
+        f.write(mRNASeq)
+        f.write('\n\nProtein sequence of ORF:\n')
+        f.write(protein)
+        f.write('\n\nPOSSIBLE EXTENSIONS\n')
+        f.write('-------\n')
+        f.write('1.) Create pipeline from ORF to homology')
+        f.write(' modeling software like SWISSMODEL.\n')
+        f.write('2.) Create pipeline searching AlphaFold with ORF.\n')
+        f.write('3.) Connect a thermostabilty engineering program ')
+        f.write('using BLOSOM 62 and swaping for thermostable\n')
+        f.write('amino acid substitutes.\n\n')
+        f.write('Thank you for visiting our program. Have a')
+        f.write(' great day!')
+
+        f.write('\n`-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+        f.write('-=-=-=-=-=-=-=-=-=-=-=-=-=')
+        f.write('-=-=-=-=-=-=-=-=-´')
+        f.close()
 
 
 def readFasta():
@@ -29,7 +89,7 @@ def dnaPol(seq):
     return reverseComp
 
 
-def rnaPol():
+def rnaPol(seq):
     frame1 = ""
     frame2 = ""
     frame3 = ""
@@ -47,8 +107,7 @@ def rnaPol():
                     break
                 else:
                     frame1 += codon
-    print("frame 1")
-    print(frame1)
+            break
     for i in range(1, len(seq), 3):
         codon = seq[i:i + 3]
         if codon == "ATG":
@@ -63,8 +122,7 @@ def rnaPol():
                     break
                 else:
                     frame2 += codon
-    print("frame 2")
-    print(frame2)
+            break
     for i in range(2, len(seq), 3):
         codon = seq[i:i + 3]
         if codon == "ATG":
@@ -79,19 +137,35 @@ def rnaPol():
                     break
                 else:
                     frame3 += codon
-    print("frame 3")
-    print(frame3)
+            break
     frameList = [frame1, frame2, frame3]
     numberList = [len(frame1), len(frame2), len(frame3)]
-    print(numberList)
     maxValue = max(numberList)
     maxIndex = numberList.index(maxValue)
     gene = frameList[maxIndex]
-    return(gene)
+    globalList.append(gene)
+    return(globalList)
+
+
+def longestORF(globalList):
+    numberList = []
+    numberList = [len(globalList[0]), len(globalList[1])]
+    maxValue = max(numberList)
+    maxIndex = numberList.index(maxValue)
+    ORF = globalList[maxIndex]
+    return(ORF)
+
+
+def mRNA(ORF):
+    table = {"A": "A", "T": "U", "G": "G", "C": "C"}
+    mRNASeq = ""
+    for i in range(0, len(ORF), 1):
+        bp = ORF[i]
+        mRNASeq += table[bp]
+    return mRNASeq
 
 
 def translate(seq):
-
     table = {
         'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
         'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
@@ -118,19 +192,15 @@ def translate(seq):
     return protein
 
 
+globalList = []
 seq = readFasta()
-print(seq)
 reverseComp = dnaPol(seq)
-print(reverseComp)
-gene = rnaPol()
-print(gene)
-protein = translate(gene)
-print(protein)
-# print(reverseComp)
+rnaPol(seq)
+rnaPol(reverseComp)
+ORF = longestORF(globalList)
+mRNASeq = mRNA(ORF)
+protein = translate(ORF)
+writeOutput()
 
-
-# prt = read_seq("amino_acid_sequence_original.txt")
-# dna = read_seq(inputfile)
-# print(dna)
-# p = translate(dna[20:935])
-# p == prt
+# Thank you for using our program.
+# hypesmadison@gmail.com
